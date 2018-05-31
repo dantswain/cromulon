18525a3aa09533de9cb1f324445213a0bd79bb44
Dan Swain <dan.t.swain@gmail.com>
2018-05-31 16:55:03 -0400

    Add form for new source

--- lib/cromulon_web/controllers/source_controller.ex
+++ lib/cromulon_web/controllers/source_controller.ex
@@ -5,6 +5,7 @@ defmodule CromulonWeb.SourceController do
 
   alias Bolt.Sips
 
+  alias Cromulon.Discovery.Postgres, as: PGDiscovery
   alias Cromulon.Schema
 
   def index(conn, _params) do
@@ -21,5 +22,17 @@ defmodule CromulonWeb.SourceController do
     result = Schema.list_source_nodes(source_uuid, bolt)
 
     render(conn, "show.html", %{source: result.source, nodes: result.nodes})
+  end
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"source" => source}) do
+    bolt = Sips.conn()
+    schema = PGDiscovery.describe_database(source["connection_string"])
+    Schema.ingest(schema, bolt)
+
+    redirect(conn, to: source_path(conn, :index))
   end
 end
